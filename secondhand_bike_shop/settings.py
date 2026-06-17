c78a2aba4a742ae40b31ce26b89ec53e8f00a995
marivigneshwaran <marisankar78@gmail.com>
2026-06-17 18:03:52 +0530

    Fix PostgreSQL driver for Render deployment

--- secondhand_bike_shop/settings.py
+++ secondhand_bike_shop/settings.py
@@ -110,9 +110,11 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
-
 MEDIA_ROOT = BASE_DIR / 'media'
 
 
